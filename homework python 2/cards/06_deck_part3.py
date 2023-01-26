bb123c2d8897eeb23091ebe3deef86ccde622a46
KuBaN658 <garkovski_dmitri@mail.ru>
2023-01-26 16:18:40 +0300

    class Deck and Cards

---
+++ homework python 2/cards/06_deck_part3.py
@@ -0,0 +1,92 @@
+import random
+from typing import Self
+
+# Начнем с создания карты
+# ♥ ♦ ♣ ♠
+VALUES = {'2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, '10': 9, 'J': 10, 'Q': 11, 'K': 12, 'A': 13}
+SUITS = {'Spades': 0.1, 'Clubs': 0.2, 'Diamonds': 0.3, 'Hearts': 0.4}
+SUITS_UNI = {
+        'Spades': '♠',
+        'Clubs': '♣',
+        'Diamonds': '♦',
+        'Hearts': '♥'
+}
+
+
+class Card:
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit    # Масть карты
+        self.point = SUITS[suit] + VALUES[value]
+
+    def __str__(self):
+        return f'{self.value}{SUITS_UNI[self.suit]}'
+
+    def __eq__(self, other_card: Self):
+        if type(other_card) == Card:
+            return self.suit == other_card.suit
+        else:
+            raise TypeError('В колоде есть что-то не похожее на карты')
+
+    def __gt__(self, other_card: Self):
+        if type(other_card) == Card:
+            return self.point > other_card.point
+        else:
+            raise TypeError('В колоде есть что-то не похожее на карты')
+
+    def __lt__(self, other_card: Self):
+        if type(other_card) == Card:
+            return self.point < other_card.point
+        else:
+            raise TypeError('В колоде есть что-то не похожее на карты')
+
+
+# Задание: Теперь создадим колоду из 52-ух карт и реализуем все методы
+class Deck:
+    def __init__(self):
+        # Список карт в колоде. Каждым элементом списка будет объект класса Card
+        self.cards = [Card(value, suit) for suit in SUITS for value in VALUES]
+        self.cards.sort()
+
+    def __str__(self):
+        # Принцип работы данного метода прописан в 00_task_deck.md
+        return f'deck[{len(self.cards)}]: ' + \
+               ', '.join([str(card) for card in self.cards])
+
+    def draw(self, qty):
+        # Принцип работы данного метода прописан в 00_task_deck.md
+        result = [self.cards.pop(0) for _ in range(qty)]
+        return result
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+
+if __name__ == '__main__':
+    deck = Deck()
+    deck.shuffle()
+    # Задачи - реализовать нативную работу с объектами:
+    # 1. Вывод колоды в терминал:
+    print(deck)  # вместо print(deck.show())
+
+    card1, card2 = deck.draw(2)
+    # 2. Вывод карты в терминал:
+    print(card1)  # вместо print(card1.to_str())
+    print(card2)
+    # 3. Сравнение карт:
+    if card1 > card2:
+        print(f"{card1} больше {card2}")
+    if card1 < card2:
+        print(f"{card1} меньше {card2}")
+
+# Это на следующее занятие.
+# 4. Итерация по колоде:
+# for card in deck:
+#     print(card)
+
+# 5. Просмотр карты в колоде по ее индексу:
+# __getitem__(self, index):
+# print(deck[6])
+
+
+# Список ВСЕХ magic-методов см. тут: http://pythonworld.ru/osnovy/peregruzka-operatorov.html
---
+++ homework python 2/cards/full_game.py
@@ -0,0 +1,63 @@
+# from deck_total import Card, Deck
+
+"""
+Cоздадим имитацию ходов в “Дурака без козырей”:
+
+1. Создайте колоду из 52 карт. Перемешайте ее.
+2. Первый игрок берет сверху 10 карт
+3. Второй игрок берет сверху 10 карт.
+4. Игрок-1 ходит:
+    4.1. игрок-1 выкладывает самую маленькую карту по "старшенству"
+    4.2. игрок-2 пытается бить карту, если у него есть такая же масть, но значением больше.
+    4.3. Если игрок-2 не может побить карту, то он проигрывает/забирает себе(см. пункт 7)
+    4.4. Если игрок-2 бьет карту, то игрок-1 может подкинуть карту любого значения, которое есть на столе.
+5. Если Игрок-2 отбился, то Игрок-1 и Игрок-2 меняются местами. Игрок-2 ходит, Игрок-1 отбивается.
+6. Выведите в консоль максимально наглядную визуализацию данных ходов (библиотека rich)
+7* Реализовать возможность добрать карты из колоды после того, как один из игроков отбился/взял в руку
+"""
+from rich.table import Table
+from rich.console import Console
+from deck_total import Deck
+
+style = "white on green"
+console = Console(width=58, style=style)
+
+dck = Deck()
+dck.shuffle()
+# Задачи - реализовать нативную работу с объектами:
+# 1. Вывод колоды в терминал:
+table = Table(width=58)
+table.add_column("Колода",
+                 style="black",
+                 justify="center")
+table.add_row(str(dck))
+console.print(table)  # вместо print(deck.show())
+
+# 3. Сравнение карт:
+card1, card2 = dck.draw(2)
+result = ''
+if card1 > card2:
+    result = "[bold]\u003e[/]"
+if card1 < card2:
+    result = "[bold]\u003c[/]"
+
+table = Table(title="Сравнение карт",
+              width=58
+              )
+
+table.add_column("Моя карта",
+                 style="black",
+                 justify="center")
+
+table.add_column("Результат",
+                 style="white",
+                 justify="center")
+
+table.add_column("Карта оппонента",
+                 justify="center",
+                 style="black")
+
+table.add_row(str(card1), result, str(card2))
+
+
+console.print(table)
